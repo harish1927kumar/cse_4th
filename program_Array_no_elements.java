58857fbc28eb4a3e3a71b9189fddfc98379fc59f
harishkumarbhagat <43933369+harishkumarbhagat@users.noreply.github.com>
2020-04-12 12:45:56 +0530

    Add files via upload

---
+++ program_Array_no_elements.java
@@ -0,0 +1,16 @@
+/* W.A.P IN JAVA TO FIND NO OF ELEMENT IN AN ARRAY*/
+
+public class findelements
+{
+    Run|Debug
+    public static void (String args[])
+    {
+        int a[][]={{1,2},{3,4}};
+        int no =0;
+        for (int b[]=no)
+            for(int c:b)
+            no++;
+            System.out.println("No of elements is "+no);
+    }
+        
+}
